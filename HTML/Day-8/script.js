47d3520dc8713bab7bdab586e0b74c2f182e232d
Yagnesh-Vaja <yvtechcoder@gmail.com>
2025-06-06 18:35:12 +0530

    commit

---
+++ HTML/Day-8/script.js
@@ -0,0 +1,7 @@
+let addBtn = document.querySelector(".addBtn");
+let cards = document.querySelector(".cards");
+console.log(addBtn);
+console.log(cards);
+addBtn.addEventListener("click", () => {
+  cards.style.padding = 0;
+});
